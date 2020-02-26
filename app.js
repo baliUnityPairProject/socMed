2ee186d121d6eceedda1b71a31b3047e11687f4e
GustiPutraJarmana <gustimertaputra@gmail.com>
2020-02-27 00:42:04 +0700

    router & lain-lain, astungkara lancar!!! :fire:

---
+++ app.js
@@ -0,0 +1,16 @@
+const projectRouter = require('./routers/projectRouter')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => { res.render('home') })
+app.get('/home', (req, res) => { res.render('home') })
+
+app.use('/', projectRouter)
+
+
+app.listen(port, () => { console.log('app listerning to port', port) })
